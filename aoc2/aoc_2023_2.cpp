31806bc8cfcbc90f8511ad45251316f5204c1e95
Jarmo Torvinen <jarmo.torvinen@iki.fi>
2023-12-03 10:08:35 +0200

    aoc2

---
+++ aoc2/aoc_2023_2.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include "file.hpp"
+
+const std::vector<std::string> numbers = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+
+auto find_leftmost_number_in_line(std::string line) -> int
+{
+    auto leftmost_position = std::string::npos;
+    auto leftmost_number = -1;
+
+    // leftmost is the smallest find() position. Need to test all to know for sure:
+    for (auto i = 0; i < numbers.size(); ++i)
+    {
+        auto position = line.find(numbers[i]);
+        if (position < leftmost_position)
+        {
+            leftmost_position = position;
+            leftmost_number = i + 1;
+        }
+    }
+
+    auto leftmost_digit = line.find_first_of("0123456789");
+
+    if (leftmost_digit < leftmost_position)
+    {
+        leftmost_number = std::stoi(line.substr(leftmost_digit, 1));
+    }
+
+    return leftmost_number;
+}
+
+auto find_rightmost_number_in_line(std::string line) -> int
+{
+    auto rightmost_position = std::string::npos;
+    auto rightmost_number = -1;
+
+    // rightmost is the smallest rfind() position. Need to test all to know for sure:
+    for (auto i = 0; i < numbers.size(); ++i)
+    {
+        auto position = line.rfind(numbers[i]);
+
+        if (position == std::string::npos)
+        {
+            continue;
+        }
+        if (position > rightmost_position || rightmost_position == std::string::npos)
+        {
+            rightmost_position = position;
+            rightmost_number = i + 1;
+        }
+    }
+
+    // we need to check also the digits
+    auto rightmost_digit = line.find_last_of("0123456789");
+
+    if ((rightmost_digit > rightmost_position || rightmost_position == std::string::npos) && rightmost_digit != std::string::npos)
+    {
+        rightmost_number = std::stoi(line.substr(rightmost_digit, 1));
+    }
+
+    return rightmost_number;
+}
+
+auto main() -> int
+{
+    std::cout << "Advent of Code 2020 - Day 2" << std::endl;
+    auto input = load_file("input.txt");
+
+    std::cout << "Input size: " << input.size() << std::endl;
+
+    auto sum = 0;
+    // iterate input lines:
+    for (auto i = input.begin(); i != input.end(); ++i)
+    {
+        // find the leftmost number word:
+        auto leftmost = find_leftmost_number_in_line(*i);
+        auto rightmost = find_rightmost_number_in_line(*i);
+
+        if (leftmost == -1 || rightmost == -1)
+        {
+            std::cout << "No integers found in line: " << *i << std::endl;
+            continue;
+        }
+
+        // take the leftmost and rightmost and combine to two digit number:
+        auto number = leftmost * 10 + rightmost;
+        sum += number;
+    }
+    std::cout << "Sum: " << sum << std::endl;
+}
